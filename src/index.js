03e22d12671dfa6bdab32260be0b7721cf9aa229
Ozawa Ryo <92570622+2000-ozawa@users.noreply.github.com>
2021-11-21 19:12:51 +0900

    タスクの追加機能 テキストのみ

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得
+  const inputText = document.getElementById("add-text").value;
+  // テキストボックス初期化
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // p生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(p);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
