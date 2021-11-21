79a479a9e25a116eb6bffde8ba079b40b8745821
Ozawa Ryo <92570622+2000-ozawa@users.noreply.github.com>
2021-11-21 21:35:43 +0900

    リスト修正 ul要素内にタスクのデータが入ったli要素を追加するように修正 追加すると"incomplete-task"idを持つ要素が複数になるバグあり

--- src/index.js
+++ src/index.js
@@ -6,19 +6,35 @@ const onClickAdd = () => {
   // テキストボックス初期化
   document.getElementById("add-text").value = "";
 
+  // test: li生成
+  const li = document.createElement("li");
+  li.className = "incomplete-task";
+
   // div生成
   const div = document.createElement("div");
   div.className = "list-row";
+
+  // test: liタグの子要素にdivを設定
+  li.appendChild(div);
+  console.log(li);
 
   // p生成
   const p = document.createElement("p");
   p.innerText = inputText;
 
+  // button生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
   // divタグの子要素に各要素を設定
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了リストに追加
-  document.getElementById("incomplete-list").appendChild(div);
+  document.getElementById("incomplete-task").appendChild(li);
 };
 
 document
